428867098d45f0384c3524f137b493f296a77d00
Richard Steinbrecht <richardsteinbrecht@hotmail.de>
2018-11-15 09:16:42 +0100

    shits gonna rly burn

--- routes/router.js
+++ routes/router.js
@@ -10,13 +10,7 @@ router.get('/henquiries', henquiry_controller.henquiries_get);
 // POST route for henquiries page
 router.post('/henquiries', henquiry_controller.henquiries_create);
 
-// POST route for henquiries page
-router.post('/henquiries', henquiry_controller.henquiries_create);
-
 // GET route for mobille register
-router.get('/login', user_controller.login_get);
-
-// GET route for logging in / register
 router.get('/login', user_controller.login_get);
 
 // POST route for logging in

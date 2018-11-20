591cc6d5bf538a6714c5261544b3c8afb33f9274
assoron <attackspam@web.de>
2018-11-20 16:45:20 +0100

    attempt #3: fixing the ios nav

--- Social30/app/navigation/navigation.component.ts
+++ Social30/app/navigation/navigation.component.ts
@@ -2,9 +2,6 @@ import { Component } from "@angular/core";
 import { RouterExtensions } from "nativescript-angular/router";
 import { ActivatedRoute } from "@angular/router";
 
-import { Page } from 'tns-core-modules/ui/page';
-
-
 @Component({
     moduleId: module.id,
     templateUrl: "./navigation.component.html",
@@ -13,10 +10,7 @@ export class NavigationComponent {
 
     constructor(
         private routerExtension: RouterExtensions,
-        private activeRoute: ActivatedRoute,
-        private page: Page) { 
-            // page.actionBar.navigationButton.visibility = 'collapse'
-        }
+        private activeRoute: ActivatedRoute) {}
 
     ngOnInit() {
         this.routerExtension.navigate([{

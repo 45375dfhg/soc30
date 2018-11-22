211cee01bdcfab3ab4d20593171de30353a9c2f3
Richard Steinbrecht <richardsteinbrecht@hotmail.de>
2018-11-22 09:14:26 +0100

    bla

--- controller/userController.js
+++ controller/userController.js
@@ -51,10 +51,10 @@ exports.register_post = function (req, res, next) {
 
 exports.login_post = function (req, res, next) {
 if (req.body.logemail && req.body.logpassword) {
-  User.findOne({ email: req.body.email }, function (err, user) {
+  User.findOne({ email: req.body.logemail }, function (err, user) {
     if (err) return res.status(500).send('Error on the server.');
     if (!user) return res.status(404).send('No user found.');
-    var passwordIsValid = bcrypt.compareSync(req.body.password, user.password);
+    var passwordIsValid = bcrypt.compareSync(req.body.logpassword, user.password);
     if (!passwordIsValid) return res.status(401).send({ auth: false, token: null });
     var token = jwt.sign({ id: user._id }, config.secret, {
       expiresIn: 86400 // expires in 24 hours

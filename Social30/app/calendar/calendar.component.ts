86092f3fdeb058d0e0fdad8ba5196989e31eea1a
assoron <attackspam@web.de>
2018-11-22 21:18:53 +0100

    grouping the calender results works

--- Social30/app/calendar/calendar.component.ts
+++ Social30/app/calendar/calendar.component.ts
@@ -4,6 +4,8 @@ import { Page } from "tns-core-modules/ui/page";
 import { Item } from "../shared/models/item";
 import { CalendarService } from "../shared/services/calendar.service";
 
+import _ from "lodash";
+
 @Component({
 	moduleId: module.id,
 	selector: 'calendar',
@@ -23,11 +25,15 @@ export class CalendarComponent implements OnInit {
         this.receiveAndOrder();
     }
 
-	
+	// this needs to be changed to date
+	// but otherwise works just fine 
     receiveAndOrder() {
         this.calendarService.getEntries().subscribe(
 			result => {
-				console.log(result);
+				let tmp = _.groupBy(result, function(entry) {
+					return entry.amountAide;
+				});
+				console.log(tmp);
 			},
 			error => console.log('receiveAndOrder failed')
 		);

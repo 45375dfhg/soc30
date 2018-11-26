1ade35f12abf6a8beda061a7e6095365951fcad1
Pascal Egner <pascalegner@Pascals-Mac-mini.local>
2018-11-26 16:37:42 +0100

    Gesuch details

--- Social30/app/henquiry-detail/henquiry.detail.component.ts
+++ Social30/app/henquiry-detail/henquiry.detail.component.ts
@@ -2,7 +2,7 @@ import { Component, ViewContainerRef } from "@angular/core";
 import { ModalDialogService } from "nativescript-angular/directives/dialogs";
 import { Page } from "tns-core-modules/ui/page";
 import { getCategoryIconSource } from "../app.component";
-import { DatePicker } from "tns-core-modules/ui/date-picker";
+import { FontStyles, PropertyEditor, RadDataForm } from "nativescript-ui-dataform";
 
 
 
@@ -13,7 +13,8 @@ import { DatePicker } from "tns-core-modules/ui/date-picker";
     styleUrls: ['./henquiry.detail.component.scss']
 })
 export class HenquiryDetailComponent {
-    
+
+
     public constructor(page: Page) { 
         //page.actionBarHidden = true;
     }
@@ -24,32 +25,7 @@ export class HenquiryDetailComponent {
     }
 
 
-    onPickerLoaded(args) {
-        let datePicker = <DatePicker>args.object;
 
-        datePicker.minDate = new Date();
-        datePicker.maxDate = new Date(new Date().getTime() + (365 * 24 * 60 * 60 * 1000));
-    }
-
-    onDateChanged(args) {
-        console.log("Date New value: " + args.value);
-        console.log("Date value: " + args.oldValue);
-    }
-
-    onDayChanged(args) {
-        console.log("Day New value: " + args.value);
-        console.log("Day Old value: " + args.oldValue);
-    }
-
-    onMonthChanged(args) {
-        console.log("Month New value: " + args.value);
-        console.log("Month Old value: " + args.oldValue);
-    }
-
-    onYearChanged(args) {
-        console.log("Year New value: " + args.value);
-        console.log("Year Old value: " + args.oldValue);
-    }
 
 
 }

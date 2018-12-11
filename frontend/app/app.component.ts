8e5e12373b3509c5c33e6b502bdcae3e2db36fb8
assoron <attackspam@web.de>
2018-12-11 16:36:50 +0100

    fixed SYSTEM_UI_FLAG_LIGHT_STATUSon sdk <23 versions

--- frontend/app/app.component.ts
+++ frontend/app/app.component.ts
@@ -1,21 +1,17 @@
 import { Component } from "@angular/core";
 import * as application from "tns-core-modules/application";
-import { isAndroid, isIOS } from "tns-core-modules/platform";
+import { isAndroid, isIOS, device } from "tns-core-modules/platform";
 import { RouterExtensions } from "nativescript-angular/router";
 
 //StatusbarColor
 import { topmost } from 'tns-core-modules/ui/frame';
 
 //Carousel
-
 import { registerElement } from 'nativescript-angular/element-registry';
 import { Carousel, CarouselItem } from 'nativescript-carousel';
 
 registerElement('Carousel', () => Carousel);
 registerElement('CarouselItem', () => CarouselItem);
-
-
-import * as app from 'application';
 
 declare var android;
 
@@ -30,8 +26,10 @@ export class AppComponent {
         if(isIOS){
             //topmost().ios.controller.navigationBar.barStyle = 1;
         } else {
-            let decorrView = app.android.startActivity.getWindow().getDecorView();
-            decorrView.setSystemUiVisibility(android.view.View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
+            let decorrView = application.android.startActivity.getWindow().getDecorView();
+            if (device.sdkVersion >= "23") {
+                decorrView.setSystemUiVisibility(android.view.View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
+            }
         }
     }
 
